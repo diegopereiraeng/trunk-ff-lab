b1dbc7daba9205c7b1425ecf11fedaf56f9dae02
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-03-06 22:56:15 -0300

    cleanup

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,20 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  // Add personalized greeting
-  const name = req.url.split('/')[1];
-  const greeting = name ? `Hello, ${name}!\n` : 'Hello, Harness!\n';
-  res.end(greeting);
-  
-  // Log request details
-  const logMessage = `${new Date().toISOString()} ${req.method} ${req.url}\n`;
-  fs.appendFile('access.log', logMessage, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-
+  res.end('Hello, Harness!\n');
 });
 
 server.listen(port, hostname, () => {
