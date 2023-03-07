38fde4070e9074a7023e08f814b3e959d1ffba73
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-03-06 22:43:50 -0300

    feature logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,6 +8,14 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, Harness!\n');
+  
+  // Log request details
+  const logMessage = `${new Date().toISOString()} ${req.method} ${req.url}\n`;
+  fs.appendFile('access.log', logMessage, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
