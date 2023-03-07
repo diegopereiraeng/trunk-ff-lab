334ad69a46d2f0d4389ee4d388cb97182de11bb3
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-03-06 22:39:20 -0300

    greeting feature

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Add personalized greeting
+  const name = req.url.split('/')[1];
+  const greeting = name ? `Hello, ${name}!\n` : 'Hello, World!\n';
+  res.end(greeting);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
